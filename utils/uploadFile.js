bb3890a2c027e50ff69d3bf39489cd03430ae177
Olaitan <olaitanmaryakano@gmail.com>
2024-08-06 17:30:33 +0100

    update

--- utils/uploadFile.js
+++ utils/uploadFile.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, uploadDir);
     },
-    folderName: function (req, file, cb) {
+    filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         cb(null, file.originalname);
     }
