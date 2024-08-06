88817e0212c91c02ec535dd52b659c3a8a526d38
Olaitan <olaitanmaryakano@gmail.com>
2024-08-06 13:55:50 +0100

    update

--- utils/uploadFile.js
+++ utils/uploadFile.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const multer = require('multer');
 const fs = require('fs');
-const path = require('path');
+
 
 // Directory to store uploads from environment variable
 const uploadDir = process.env.UPLOAD_DIR;
@@ -12,7 +13,7 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, uploadDir);
     },
-    filename: function (req, file, cb) {
+    folderName: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         cb(null, file.originalname);
     }
