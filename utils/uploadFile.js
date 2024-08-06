9d890b263c91cf910cb0419e038d4c3eba926ac3
Olaitan <olaitanmaryakano@gmail.com>
2024-08-06 19:07:40 +0100

    update

--- utils/uploadFile.js
+++ utils/uploadFile.js
@@ -1,14 +1,15 @@
-const path = require('path');
-const multer = require('multer');
 const fs = require('fs');
+const path = require('path');
 
+// Directory to store uploads from environment variable or default to './uploads'
+const uploadDir = process.env.UPLOAD_DIR || path.join(__dirname, 'uploads');
 
-// Directory to store uploads from environment variable
-const uploadDir = process.env.UPLOAD_DIR;
+// Ensure the directory exists
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
 
+// Your existing multer configuration
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, uploadDir);
