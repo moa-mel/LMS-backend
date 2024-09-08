dcd83b63d9cf86a5905e4ee1e6d529857faa9467
Olaitan <olaitanmaryakano@gmail.com>
2024-09-08 21:29:48 +0100

    update

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors')
 const fellowRouter = require("./routes/fellowRoute");
 const adminRoute = require("./routes/adminRoute");
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 
 const app = express();
@@ -30,6 +32,28 @@ app.use('/api/admin', adminRoute);
 mongoose.connect(process.env.DB_CONNECT)
   .then(() => console.log("Connected to the database"))
   .catch((err) => console.error("Unable to connect to the database", err));
+
+  const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Product lab backend endpoints',
+        version: '1.0.0',
+      },
+      servers: [
+        {
+          url: "https://lms-backend-km3b.onrender.com/"
+        }
+      ]
+    },
+    apis: ['./routes/*.js'], // files containing annotations as above
+  }
+
+const specs = swaggerJsdoc(options)
+app.use('/api-docs',
+   swaggerUi.serve, 
+   swaggerUi.setup(specs)
+  );
 
 // Error handling middleware
 app.use((err, req, res, next) => {
